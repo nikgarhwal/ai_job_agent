9d81275a8d68dc81638ad435b15dc1cf9e393d0c
Nikhil <121622923+nikgarhwal@users.noreply.github.com>
2025-09-04 15:20:19 +0530

    Add job search base class

---
+++ job_search/base.py
@@ -0,0 +1,10 @@
+"""
+Base class for job searching functionality.
+"""
+
+class JobSearchBase:
+    def search_jobs(self, profile):
+        """
+        Search jobs based on profile.
+        """
+        pass
