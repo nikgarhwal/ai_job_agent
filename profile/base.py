f7ba5be0721d23943932042b6f807af5f37e3bfa
Nikhil <121622923+nikgarhwal@users.noreply.github.com>
2025-09-04 15:21:11 +0530

    Add profile base class

---
+++ profile/base.py
@@ -0,0 +1,10 @@
+"""
+Base class for profile parsing and matching.
+"""
+
+class ProfileBase:
+    def parse_profile(self, resume_file, github_url=None):
+        """
+        Parse and extract data from resume and GitHub.
+        """
+        pass
