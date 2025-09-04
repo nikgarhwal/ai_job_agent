4c8a97e780d5dba75044240d0c7e478822b675b5
Nikhil <121622923+nikgarhwal@users.noreply.github.com>
2025-09-04 15:20:50 +0530

    Add reply base class

---
+++ reply/base.py
@@ -0,0 +1,10 @@
+"""
+Base class for recruiter reply functionality.
+"""
+
+class ReplyBase:
+    def auto_reply(self, message, profile):
+        """
+        Generate and send auto-reply based on incoming message and profile.
+        """
+        pass
