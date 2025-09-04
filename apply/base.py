eae6a4a8a306e792cc490910c3c11557c924428c
Nikhil <121622923+nikgarhwal@users.noreply.github.com>
2025-09-04 15:20:36 +0530

    Add job application base class

---
+++ apply/base.py
@@ -0,0 +1,10 @@
+"""
+Base class for job application functionality.
+"""
+
+class JobApplyBase:
+    def apply_to_job(self, job, profile):
+        """
+        Apply to a given job using profile.
+        """
+        pass
